66ae9962cf39fbc3972ff952b67f4b7eb8e0eda0
Andrespiedrahita98 <andrespiedrahita98@hotmail.com>
2023-04-30 15:37:54 -0500

    error

--- Question.py
+++ Question.py
@@ -6,5 +6,5 @@ fecha_actual = datetime.datetime.now()
 # Extraer el día de la fecha actual y guardarlo en una variable
 dia_actual = fecha_actual.day
 
-print("La fecha actual es:", fecha_actual)
+print("La fecha actual es:", f)
 print("El día actual es:", dia_actual)
